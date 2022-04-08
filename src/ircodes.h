76666ffb1754836d746a03044970b156e13305f4
chrispab <cbattisson@gmail.com>
2022-04-09 00:48:22 +0100

    lookuyp

--- src/ircodes.h
+++ src/ircodes.h
@@ -1,39 +1,83 @@
 #ifndef _IRCODES_H
 #define _IRCODES_H
-power on      0xE13EA45B
-power off     0xE13E13EC
-cd 0xE13EA15E
-tuner   0xE13EBB44
-aux     0xE13ED926
-disc 0xE13E916E
-tape 1 mon 0xE13EB14E
-tape 2 0xE13E8976
-video 1 0xE13E43BC
-video 2 0xE13E03FC
-video 3 0xE13E837C
+
+// power on      0xE13EA45B
+#define POWER_ON 0xE13EA45B
+
+// power off     0xE13E13EC
+#define POWER_OFF 0xE13E13EC
+
+// cd 0xE13EA15E
+#define CD 0xE13EA15E
+
 // tuner   0xE13EBB44
-#define SELECT_TUNER 0xE13EBB44
+#define TUNER 0xE13EBB44
 
 // aux     0xE13ED926
-#define SELECT_AUX 0xE13ED926
+#define AUX 0xE13ED926
+
+// disc 0xE13E916E
+#define DISC 0xE13E916E
+
+// tape 1 mon 0xE13EB14E
+#define TAPE_1_MON 0xE13EB14E
+
+// tape 2 0xE13E8976
+#define TAPE_2 0xE13E8976
 
 // video1  0xE13E43BC
-#define SELECT_VIDEO1 0xE13E43BC
+#define VIDEO1 0xE13E43BC
 
-// vol up  0xE13E11EE
+// video 2 0xE13E03FC
+#define VIDEO2 0xE13E03FC
+
+// video 3 0xE13E837C
+#define VIDEO3 0xE13E837C
+
+// 1 0xE13E30CF
+// 2 0xE13EB04F
+// 3 0xE13E708F
+// 4 0xE13EF00F
+// 5 0xE13E08F7
+// 6 0xE13E8877
+// 7 0xE13E48B7
+// 8 0xE13EC837
+// 9 0xE13EA857
+// 10 0xE13E32CD
+// 0 0xE13E6897
+// +10 0xE13EB24D
+
+// tune mode 0xE13ECC33
+// display 0xE13E649B
+
+// vol + 0xE13E11EE
 #define VOLUME_UP 0xE13E11EE
 
-// vol dn  0xE13E31CE
-#define VOLUME_DOWN 0xE13E31CE
+// tune - 0xE13E8B74
 
 // mute    0xE13E29D6
 #define MUTE 0xE13E29D6
 
-// on      0xE13EA45B
-#define POWER_ON 0xE13EA45B
+// tune + 0xE13E4BB4
 
-// off     0xE13E13EC
-#define POWER_OFF 0xE13E13EC
+// vol dn  0xE13E31CE
+#define VOLUME_DOWN 0xE13E31CE
 
+// spk a 0xE13E738C
+// spk b 0xE13EF30C
 
+// open/close 0xE13E12ED
+// stop 0xE13E40BF
+// play 0xE13E807F
+// skip back 0xE13EA05F
+// skip forward 0xE13E609F
+// pause 0xE13E52AD
+// scan back 0xE13E20DF
+// scan forward 0xE13EE01F
+// next disc 0xE13EE817
+// time 0xE13E10EF
+// repeat 0xE13E50AF
+// random 0xE13EC03F
+// program 0xE13ED02F
+// delete 0xE13E0AF5
 #endif
